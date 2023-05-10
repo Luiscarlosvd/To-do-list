138cb3d453e869afd12bd3fca21fa31f20c28184
unknown <gabrielrozas1996@gmail.com>
2023-05-10 10:51:06 -0800

    Add moch

--- test/listClass.test.js
+++ test/listClass.test.js
@@ -1,5 +1,23 @@
 import taskList from "./tasklistTest";
-jest.mock('../__mocks__/localStorage.js');
+// Mocking localStorage
+const localStorageMock = (() => {
+  let store = {};
+  return {
+    getItem: (key) => {
+      return store[key] || null;
+    },
+    setItem: (key, value) => {
+      store[key] = value.toString();
+    },
+    removeItem: (key) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    },
+  };
+})();
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
 
 describe('taskList', () => {
     // Add Method
