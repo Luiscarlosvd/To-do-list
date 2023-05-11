1bbe9b45376e436fd79276ad74bd4c87428ab7dc
Luiscarlosvd <lc26cr7@gmail.com>
2023-05-11 11:07:37 -0400

    Create test for clearCompletedTask function

--- test/listClass.test.js
+++ test/listClass.test.js
@@ -59,4 +59,42 @@ describe('taskList', () => {
     // Assert
     expect(list.list).not.toContain(task);
   });
+
+  // Clear Completed Task
+  test('returns filtered tasklist with all objects that were not completed', () => {
+    // Arrange
+    const list = new TaskList();
+    const task = {
+      description: 'Clean my bedroom',
+      completed: false,
+      index: 1,
+    };
+    const task2 = {
+      description: 'Wash the dishes',
+      completed: true,
+      index: 2,
+    };
+    const task3 = {
+      description: 'Play videogames',
+      completed: false,
+      index: 3,
+    };
+    const task4 = {
+      description: 'Seek whales',
+      completed: true,
+      index: 4,
+    };
+    list.addTask(task);
+    list.addTask(task2);
+    list.addTask(task3);
+    list.addTask(task4);
+
+
+    // Act
+    list.clearCompletedTask();
+
+    // Assert
+    expect(list.list).not.toContain(task2);
+    expect(list.list).not.toContain(task4);
+  });
 });
